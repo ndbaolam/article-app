317f20682ed3a06fe7df4c1c81ce4b0ab242ac17
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-03-30 19:23:19 +0700

    Cau truc folder typeDefs, resolvers

---
+++ resolvers/category.resolvers.ts
@@ -0,0 +1,48 @@
+import Category from "../models/category.model";
+
+export const resolversCategory = {
+  Query: {
+    getListCategories: async () => {
+      const categories = await Category.find({
+        deleted: false
+      });
+
+      return categories;
+    }
+  },
+  Mutation: {
+    createCategory: async (_, args) => {
+      const { category } = args;
+
+      const record = new Category(category);
+      await record.save();
+
+      return record;
+    },
+    deleteCategory: async (_, args) => {
+      const { id } = args;
+
+      await Category.updateOne({
+        _id: id
+      }, {
+        deleted: true,
+        deletedAt: new Date()
+      });
+
+      return "Đã xóa!";
+    },
+    updateCategory: async (_, args) => {
+      const { id, category } = args;
+
+      await Category.updateOne({
+        _id: id
+      }, category);
+
+      const newData = await Category.findOne({
+        _id: id
+      }); 
+
+      return newData;
+    },
+  }
+}
--- typeDefs/article.typeDefs.ts
+++ typeDefs/article.typeDefs.ts
@@ -1,6 +1,6 @@
 import { gql } from "apollo-server-express";
 
-export const typeDefs = gql`
+export const typeDefsArticle = gql`
   type Article {
     id: ID,
     title: String,
@@ -9,35 +9,20 @@ export const typeDefs = gql`
     category: Category
   }
 
-  type Category {
-    id: ID,
-    title: String,
-    avatar: String
-  }
-
   type Query {
     getListArticles: [Article]
-    getListCategories: [Category]
   }
 
   input ArticleInput {
     title: String,
     avatar: String,
     description: String
-  }
-
-  input CategoryInput {
-    title: String,
-    avatar: String
+    categoryId: String
   }
 
   type Mutation { 
     createArticle(article: ArticleInput): Article
     deleteArticle(id: ID): String
     updateArticle(id: ID, article: ArticleInput): Article 
-
-    createCategory(category: CategoryInput): Category
-    deleteCategory(id: ID): String
-    updateCategory(id: ID, category: CategoryInput): Category
   }
 `;
